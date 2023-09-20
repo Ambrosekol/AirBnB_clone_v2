0d9fb97c28faa3cf8bfe144cffadcfaa71136481
Lucas Sangura <sanguraowens@gmail.com>
2023-09-20 23:28:31 +0300

    Modify all and delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        return {key: value for key, value in self.__objects.items() if type(value) is cls}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -55,4 +57,4 @@ class FileStorage:
             return
         key = obj.to_dict()['__class__'] + '.' + obj.id
         if key in self.__objects:
-            del self.__objects[{key: obj}]
+            del self.__objects[key]
