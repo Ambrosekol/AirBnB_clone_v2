73107a6592cb9351f3e9476395be6d8a96ef3226
ambrose <kolus7381@gmail.com>
2023-10-20 21:15:10 +0100

    new

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,7 @@ def c_txt_input(text):
     return "C {}".format(escape(newval))
 
 
-@app.route("/python/", defaults={'text': "is_cool"}, strict_slashes=False, )
+@app.route("/python/", defaults={'text': "is_cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_txt_input(text):
     newchar = text.replace('_', ' ')
