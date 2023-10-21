67bfc93e11a0406d41ea543d41601d920d05f956
ambrose <kolus7381@gmail.com>
2023-10-21 12:03:10 +0100

    new

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+"""
+This is a python a script that starts a Flask web application
+1. ALX task 0
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_txt_input(text):
+    newval = text.replace('_', ' ')
+    return "C {}".format(escape(newval))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_integer(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def n_template(n):
+    return "<h1>Number: {}</h1>".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
